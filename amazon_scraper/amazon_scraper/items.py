f32412b6884c623890c978e2eeed9b22def77d85
nova-sangeeth <novasangeeth@outlook.com>
2020-12-05 18:48:08 +0530

    added added new items for review and profile.

--- amazon_scraper/amazon_scraper/items.py
+++ amazon_scraper/amazon_scraper/items.py
@@ -8,6 +8,17 @@ import scrapy
 
 class AmazonScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    
-    pass
+    reviewer = scrapy.Field()
+    rating = scrapy.Field()
+    title = scrapy.Field()
+    date_of_review = scrapy.Field()
+    verified_review = scrapy.Field()
+    review = scrapy.Field()
+    votes = scrapy.Field()
+    comments = scrapy.Field()
+
+
+class AmazonProfileItem(scrapy.Item):
+    review_ranking = scrapy.Field()
+    number_of_votes = scrapy.Field() 
+    ranking = scrapy.Field()
