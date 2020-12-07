923c4e980d888186a078e72e697fdd9efc8a5b3c
nova-sangeeth <novasangeeth@outlook.com>
2020-12-07 19:38:30 +0530

    added new pipline to output a file in .csv

--- amazon_scraper/amazon_scraper/pipelines.py
+++ amazon_scraper/amazon_scraper/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
-
+from scrapy.exporters import CsvItemExporter, JsonItemExporter
+from .items import AmazonScraperItem
+from .spiders import review
+import unicodedata
 class AmazonScraperPipeline:
-    def process_item(self, item, spider):
-        return item
+    def __init__(self):
+        self.file = open("items3.csv", 'wb')
+        self.exporter = CsvItemExporter(self.file)
+        # self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+    
+    def close_spider(self, review):
+        self.exporter.finish_exporting()
+        self.file.close()
+        
+    def process_item(self, AmazonScraperItem, review):
+        self.exporter.export_item(AmazonScraperItem)
+        return AmazonScraperItem
+   
