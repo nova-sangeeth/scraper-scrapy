5f0656d7125cb8143e1801f5a29f7587a296a848
nova-sangeeth <novasangeeth@outlook.com>
2020-12-11 03:03:42 +0530

    profile spider.

--- amazon_scraper/amazon_scraper/spiders/profile.py
+++ amazon_scraper/amazon_scraper/spiders/profile.py
@@ -1,22 +1,28 @@
 import scrapy
-
+import json
 from ..items import ProfileItem
-
+filename = '/home/nova/webdev-lessons/web_scraper/amazon_scraper/amazon-profile-url.txt'
 class ProfileSpider(scrapy.Spider):
     name = 'profile'
     allowed_domains = ['amazon.in']
-    start_urls = ['https://www.amazon.in/gp/profile/amzn1.account.AFJGRXKY5QXLUXOS2NHDW3FKNSEA/ref=cm_cr_arp_d_gw_btm?ie=UTF8']
-    @staticmethod
-    def get_text(selector_list):
-        return "".join(selector_list).replace("\n", "").strip()
+    # start_urls = [
+    #     'https://www.amazon.in/hz/gamification/api/contributor/dashboard/amzn1.account.AGHOJQ4IOQAGANGL6RYNRO7SYADA',
+    #     'https://www.amazon.in/hz/gamification/api/contributor/dashboard/amzn1.account.AEJH7F5LBTU6KDEECRJUZIVSZINA',
+    #     'https://www.amazon.in/hz/gamification/api/contributor/dashboard/amzn1.account.AEWUQHUYUCFGCMUBAGGKWUY27TKA',
+    #     'https://www.amazon.in/hz/gamification/api/contributor/dashboard/amzn1.account.AEWUQHUYUCFGCMUBAGGKWUY27TKA',
+    #     'https://www.amazon.in/hz/gamification/api/contributor/dashboard/amzn1.account.AH6LMXTC7N4EZNCFG2SEBOWTJFUQ',
+    #     ]
+    def __init__(self, filename=filename):
+        if filename:
+                with open(filename, 'r') as r:
+                    self.start_urls = r.readlines()
 
-    def parse(self, response):
+    def parse (self, response):
         item = ProfileItem()
-        for profile in response.css(".desktop padded card dashboard-desktop-card").extract():
-            helpful_votes = profile.css(".a-profile-name ::text").extract()
-            item["helpful_votes"] = self.get_text(helpful_votes)
-
-            yield item
-
-
-
+        resp = json.loads(response.body)
+        helpful = resp.get('helpfulVotes').get('helpfulVotesData').get('count')
+        item['helpful_votes'] = helpful
+        helpful = resp.get('reviews').get('reviewsCountData').get('count')
+        item['total_review'] = helpful
+        yield item
+            
