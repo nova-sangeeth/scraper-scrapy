2329df879f281466f694f55e247974413b34b02b
nova sangeeth <novasangeeth@outlook.com>
2020-12-20 00:13:58 +0530

    new spider to crawl the ranking only from the given list of urls from the reviews.

--- amazon_scraper/amazon_scraper/spiders/ranking.py
+++ amazon_scraper/amazon_scraper/spiders/ranking.py
@@ -1,10 +1,22 @@
 import scrapy
-
-
+from scrapy import item
+from ..items import rankingItem
+import json
+import logging
 class RankingSpider(scrapy.Spider):
     name = 'ranking'
     allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/']
+    start_urls = ['https://www.amazon.com/profilewidget/bio/amzn1.account.AEQWFRM5IA7FG7DCJTEWAPYUOSAQ?view=visitor']
 
     def parse(self, response):
-        pass
+        items = rankingItem()
+        resp = json.loads(response.body)
+        try:
+            ranking = resp.get('topReviewerInfo').get('rank')
+            items['ranking'] = ranking
+        except:
+            logging.warning('There is no data in the ranking section....')
+        yield items
+
+        
+            
