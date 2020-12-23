c77471172af6395163382828302e376a511a5d76
nova sangeeth <novasangeeth@outlook.com>
2020-12-23 23:25:34 +0530

    changes to the profile spider.

--- amazon_scraper/amazon_scraper/spiders/profile.py
+++ amazon_scraper/amazon_scraper/spiders/profile.py
@@ -3,10 +3,12 @@ import json
 from ..items import ProfileItem
 import logging
 
-filename = '/home/novasangeeth/Code--dev/scraper-scrapy/amazon_scraper/dump/disney_forge_profile_urls.txt'
+filename = '/home/novasangeeth/Code--dev/scraper-scrapy/amazon_scraper/url_dump/think_fun_profile.txt'
+
 class ProfileSpider(scrapy.Spider):
     name = 'profile'
     allowed_domains = ['amazon.com']
+    # start_urls = []
     def __init__(self, filename=filename):
         if filename:
                 with open(filename, 'r') as r:
@@ -16,21 +18,21 @@ class ProfileSpider(scrapy.Spider):
         item = ProfileItem()
         resp = json.loads(response.body)
         try:
-            ranking = resp.get('topReviewerInfo').get('rank')
-            item['ranking'] = ranking
-        except:
-            logging.warning('Ranking is not available')
-        try:
             helpful = resp.get('helpfulVotes').get('helpfulVotesData').get('count')
             item['helpful_votes'] = helpful
         except:
-            logging.warning('Helpful votes not available..')
+            logging.debug('Helpful votes not available..')
         try:
             helpful = resp.get('reviews').get('reviewsCountData').get('count')
             item['total_review'] = helpful
         except:
-            logging.warning('total_review  not available..')
+            logging.debug('total_review  not available..')
+        try:
+            item['page_url'] = response.request.url
+        except:
+            logging.debug('The url is not available.')
+
         yield item
             
 # THE AJAX REQUEST URL FOR THE RANKING IN A PARTICULAR PROFILE IS:(replace the necessary account and run it in a file.)
-# https://www.amazon.com/profilewidget/bio/amzn1.account.AEQWFRM5IA7FG7DCJTEWAPYUOSAQ?view=visitor
+# https://www.amazon.com/hz/gamification/api/contributor/dashboard/amzn1.account.AHZLBXPEMVGLNEEXYCZ4FHXAIWEA
