c2dc2001dcf18dc8520ec733cb4e76c9a36c0dad
nova sangeeth <novasangeeth@outlook.com>
2020-12-24 02:38:16 +0530

    Changes to the setting like: new proxy pool new download delay

--- amazon_scraper/amazon_scraper/settings.py
+++ amazon_scraper/amazon_scraper/settings.py
@@ -13,7 +13,7 @@ SPIDER_MODULES = ['amazon_scraper.spiders']
 NEWSPIDER_MODULE = 'amazon_scraper.spiders'
 PROXY_POOL_ENABLED = True
 
-
+max_retry_times = 10
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'amazon_scraper (+http://www.yourdomain.com)'
 # USER_AGENT = 'Mozilla/5.0 (compatible;Googlebot/2.1; +http://www.google.com/bot.html)'
@@ -21,17 +21,17 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 # USER_AGENT ='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
-
+DEPTH_LIMIT = 1000
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS = 10
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 20
+# CONCURRENT_REQUESTS_PER_IP = 20
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -44,7 +44,13 @@ DOWNLOAD_DELAY = 10
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 #}
-
+# ROTATING_PROXY_LIST = [
+#    #  'proxy1.com:8000',
+#    #  'proxy2.com:8031',
+#     # ...
+# ]
+# ROTATING_PROXY_LIST_PATH = '/home/novasangeeth/Code--dev/scraper-scrapy/amazon_scraper/amazon_scraper/proxy-list.txt'
+ROTATING_PROXY_LIST_PATH = '/home/novasangeeth/Code--dev/scraper-scrapy/amazon_scraper/amazon_scraper/proxy-lists/google-approved-list.txt'
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 SPIDER_MIDDLEWARES = {
@@ -55,8 +61,12 @@ SPIDER_MIDDLEWARES = {
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
    'amazon_scraper.middlewares.AmazonScraperDownloaderMiddleware': 543,
-   'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
-   'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+   # 'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+   # 'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+   'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+   'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+   'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+   'rotating_proxies.middlewares.BanDetectionMiddleware': 620
 }
 
 # Enable or disable extensions
@@ -75,14 +85,14 @@ ITEM_PIPELINES = {
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 3
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 10
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
