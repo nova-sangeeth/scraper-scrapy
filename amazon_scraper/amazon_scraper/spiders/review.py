ebed9684149291c1da8a9b768fd16d780873f47e
nova_sangeeth <novasangeeth@outlook.com>
2021-09-10 23:55:52 +0530

    formated the review spider.

--- amazon_scraper/amazon_scraper/spiders/review.py
+++ amazon_scraper/amazon_scraper/spiders/review.py
@@ -5,30 +5,32 @@ import logging
 
 filename = ''
 
+
 class ReviewSpider(scrapy.Spider):
     name = "review"
     allowed_domains = ["amazon.com"]
 
     if filename == '':
         start_urls = []
-        
+
     def __init__(self, filename=filename):
         if filename:
             with open(filename, 'r') as r:
-                self.start_urls= r.readlines() 
+                self.start_urls = r.readlines()
 
 # To scrape a specific number of pages of reviews.
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------------------------
     # BaseUrl='https://www.amazon.com/product-reviews/TESTONLY/ref=zg_bs_7586146011_cr_10?ie=UTF8&refRID=TESTONLY&pageNumber='
     # start_urls = []
     # for i in range(0,50):
     #     start_urls.append(BaseUrl+str(i))
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
     def get_text(selector_list):
         return "".join(selector_list).replace("\n", "").strip()
     # working on the empty data and depth of the scraping that happens during a session
+
     def parse(self, response):
         item = AmazonScraperItem()
         for review in response.css(".review"):
@@ -36,13 +38,15 @@ class ReviewSpider(scrapy.Spider):
             author = review.css(".a-profile-name ::text").extract()
             item["author"] = self.get_text(author)
 
-            author_profile_url = review.css(" a.a-profile ::attr(href)").extract()
+            author_profile_url = review.css(
+                " a.a-profile ::attr(href)").extract()
             item["author_profile_url"] = self.get_text(author_profile_url)
 
             title = review.css(".review-title-content span ::text").extract()
             item["title"] = self.get_text(title)
 
-            review_content = review.css(".review-text-content ::text").extract()
+            review_content = review.css(
+                ".review-text-content ::text").extract()
             item["review_content"] = self.get_text(review_content)
 
             verified = review.css(".review-format-strip ::text").extract()
@@ -57,10 +61,12 @@ class ReviewSpider(scrapy.Spider):
             votes_helpful = review.css(".cr-vote-text ::text").extract()
             item["votes_helpful"] = self.get_text(votes_helpful)
 
-            image_num = review.css(".review-image-tile-section ::attr(src)").extract()
+            image_num = review.css(
+                ".review-image-tile-section ::attr(src)").extract()
             item["image_num"] = len(image_num)
 
-            num_of_comments = review.css(".review-comment-total ::text").extract()
+            num_of_comments = review.css(
+                ".review-comment-total ::text").extract()
             item["num_of_comments"] = self.get_text(num_of_comments)
 
             item['pageURL'] = redirect_urls[0] if redirect_urls else response.request.url
